8e8b325b615e1dcd2121a102836e6c88450df30b
Na Lin <lna483018@gmail.com>
2023-02-13 20:58:55 -0500

    connect api to store

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,17 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 
 import { userApi } from "./api/userApi";
+import { albumApi } from "./api/albumApi";
 
 const store = configureStore({
   reducer: {
     [userApi.reducerPath]: userApi.reducer,
+    [albumApi.reducerPath]: albumApi.reducer,
   },
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(userApi.middleware);
+    return getDefaultMiddleware()
+      .concat(userApi.middleware)
+      .concat(albumApi.middleware);
   },
 });
 
@@ -17,3 +21,4 @@ setupListeners(store.dispatch);
 export default store;
 
 export { useFetchUsersQuery } from "./api/userApi";
+export { useFetchAlbumsQuery } from "./api/albumApi";

a012b474a4c8e96333d5f41bad139f1721b426f9
Na Lin <lna483018@gmail.com>
2023-02-13 20:33:40 -0500

    create RTK Query for user

---
+++ src/store/api/userApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const userApi = createApi({
+  reducerPath: "users",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3005",
+  }),
+  endpoints(builder) {
+    return {
+      fetchUsers: builder.query({
+        query: () => {
+          return {
+            url: "/user",
+            method: "GET",
+          };
+        },
+      }),
+    };
+  },
+});
+export { userApi };
+export const { useFetchUsersQuery } = userApi;
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
+import { userApi } from "./api/userApi";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    [userApi.reducerPath]: userApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(userApi.middleware);
+  },
 });
 
+setupListeners(store.dispatch);
+
 export default store;
+
+export { useFetchUsersQuery } from "./api/userApi";
